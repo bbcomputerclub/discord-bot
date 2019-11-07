b03cb246b5d160c5a73b145e7460259e05001ded
Computer Club <bbcomputerclub1@gmail.com>
2019-11-07 04:04:55 +0000

    Add reputation

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import discord
 
 client = discord.Client()
+
+reputation = dict()
+
 @client.event
 async def on_message(msg):
 	if msg.author == client.user:
 		return
 
-	if msg.content.strip().lower() == "dab":
+	if msg.content.strip(" \n\t!?*_").lower() == "dab":
+		reputation[msg.author.id] = reputation.get(msg.author.id, 0) - 1
 		await msg.channel.send("no dabbing allowed!")
+	elif msg.content.strip().lower().startswith("^rep"):
+		if len(msg.mentions) == 0: # everyone
+			outmsg = ""
+			# TODO: don't @ everyone
+			# just lookup name
+			# also use ```
+			for user, rep in reputation.items():
+				outmsg += "<@" + str(user) + "> has " + str(rep) + " rep"
+			if len(outmsg) != 0:
+				await msg.channel.send(outmsg)
+			else: # no reps :(
+				await msg.channel.send("no reputation")
+		else:
+			rep = reputation.get(msg.mentions[0].id, 0)
+			await msg.channel.send("<@" + str(msg.mentions[0].id) + "> has " + str(rep) + " rep")
 
-client.run("NjQxODMwNTM2NjkxNzEyMDIx.XcOQ2w.4iCltuiaz3OIJMADE-OBdAK3kdc")
+client.run("NjQxODMwNTM2NjkxNzEyMDIx.XcOU_Q.4HG_0yz48HcuyGjjO2yQb1io6CM")
